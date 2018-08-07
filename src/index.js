668651bf99decd8da567347c31ecdc755f9b19cf
Lemarks <losei1@umbc.edu>
2018-08-07 06:03:00 -0400

    applied Redux to api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux';
-import { createLogger } from 'redux-logger'
-import { searchRobots } from './reducers';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk'
+import { searchRobots, requestRobots } from './reducers';
 import './index.css';
 import App from './containers/App'
 import registerServiceWorker from './registerServiceWorker';
@@ -11,7 +12,9 @@ import 'tachyons';
 
 const logger = createLogger();
 
-const store = createStore(searchRobots, applyMiddleware(logger))
+const rootReducer = combineReducers({ searchRobots, requestRobots})
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
     <Provider store={store} >
